a33d316ad3d4b9ebfbea1ee3409a15df7203416d
igorshimauti <igorshimauti@gmail.com>
2022-06-23 20:45:15 -0300

    removido excesso de espaçamento entre linhas

--- src/main/java/br/com/cadastro/web/controller/ClienteController.java
+++ src/main/java/br/com/cadastro/web/controller/ClienteController.java
@@ -15,27 +15,23 @@ public class ClienteController {
 
     @Autowired
     private ClienteService clienteService;
-
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Cliente> incluir(@RequestBody Cliente cliente) {
         Cliente clienteIncluido = clienteService.salvar(cliente);
         URI location = URI.create("cliente/" + cliente.getId());
         return ResponseEntity.created(location).body(clienteIncluido);
     }
-
     @GetMapping(value = "/{id}")
     public ResponseEntity<Cliente> recuperarPorId(@PathVariable Long id) {
         Cliente clienteRecuperado = clienteService.recuperarPorId(id);
         return ResponseEntity.ok().body(clienteRecuperado);
     }
-
     @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Cliente> atualizar(@PathVariable Long id, @RequestBody Cliente cliente) {
         cliente.setId(id);
         Cliente clienteAtualizado = clienteService.salvar(cliente);
         return ResponseEntity.ok().body(clienteAtualizado);
     }
-
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<Void> excluir(@PathVariable Long id) {
         clienteService.excluir(id);
